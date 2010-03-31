1ec3e420e05a4bf4a7e0d798e4aa5f81f03b24ba
a_trujillo05@berkeley.edu <a_trujillo05@berkeley.edu@4fb5bb12-f92f-0152-51da-c2a16c96a9b1>
2010-03-31 02:55:26 +0000

    testing--uploading skeleton xcode, no changes on it

---
+++ trunk/skater/Classes/FirstViewController.h
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.h
+//  skater
+//
+//  Created by Annette Trujillo on 3/30/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FirstViewController : UIViewController {
+
+}
+
+@end
